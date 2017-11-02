5e4d5af3963d42a05a504f6c15964cb35df938e5
dceoy <d.narsil@gmail.com>
2017-11-03 05:30:06 +0900

    Improve qc

--- procread/task.py
+++ procread/task.py
@@ -2,27 +2,28 @@
 
 import logging
 import os
-import re
 from itertools import product
 from .util import sh_c
 
 
 def do_qc_checks(config, work_dir, cpus):
     logging.info('Start quality control checks for fastq files')
+
     qc_dir = os.path.join(work_dir, 'qc')
-    logging.debug('qc_dir: {}'.format(qc_dir))
     os.makedirs(qc_dir, exist_ok=True)
+    logging.debug('qc_dir: {}'.format(qc_dir))
+    qc_log_txt = os.path.join(qc_dir, 'fastqc_log.txt')
+
     for t, r in product(['foreground', 'background'], ['read1', 'read2']):
-        fq = config['fastq'][t][r]
-        logging.debug('fq: {}'.format(fq))
-        qc_fq_dir = os.path.join(
-            qc_dir, re.sub(r'\.fastq\.gz$', '', os.path.basename(fq))
+        cmd = (
+            'fastqc --nogroup --threads {0} --outdir {1} {2}'
+            ' 2>&1 | tee -a {3}'
+        ).format(
+            cpus, qc_dir, config['fastq'][t][r], qc_log_txt
         )
-        logging.debug('qc_fq_dir: {}'.format(qc_fq_dir))
-        os.makedirs(qc_fq_dir, exist_ok=True)
-        sh_c('fastqc --threads {0} --nogroup --outdir {1} {2} > {3}'.format(
-            cpus, qc_fq_dir, fq, os.path.join(qc_fq_dir, 'fastqc.log')
-        ))
+        with open(qc_log_txt, 'a') as f:
+            f.write('$ {0}{1}'.format(cmd, os.linesep))
+        sh_c(cmd)
 
 
 def trim_adapters(config, work_dir):
--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ from procread import __version__
 setup(
     name='procread',
     version=__version__,
+    description='Read-to-variant pipeline tool for DNA-seq analyses',
     packages=find_packages(),
+    author='Daichi Narushima',
+    author_email='d.narsil@gmail.com',
+    url='https://github.com/dceoy/procread',
+    include_package_data=True,
     install_requires=[
         'biopython',
         'docopt',
@@ -16,4 +21,13 @@ setup(
     entry_points={
         'console_scripts': ['procread=procread.main:main']
     },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers'
+    ]
 )
